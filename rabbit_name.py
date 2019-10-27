bab22578db75a1c31dee7700f7e0e59d98f42be6
mbonsey <55725868+mbonsey@users.noreply.github.com>
2019-10-27 13:57:23 +1300

    Delete rabbit_name.py

--- rabbit_name.py
+++
@@ -1,4 +0,0 @@
-# Ask the user to name their rabbit and set this as the variable rabbit_name
-rabbit_name = input("Please type what you would like to call your rabbit here: ").strip().capitalize()
-
-print(rabbit_name)
