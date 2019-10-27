1d166ca9a00e7e67b67d86029ffb01c1b819064b
MBonsey <mbonsey@storans.school.nz>
2019-10-27 14:53:39 +1300

    Create a dictionary with each exercise that the user can choose from

--- main_menu.py
+++ main_menu.py
@@ -6,3 +6,6 @@ FOOD_DICT = {1: "kale", 2: "broccoli", 3: "apple"}
 
 # Create a dictionary with each food and the number of kilograms it will add to the weight of the rabbit
 FOOD_WEIGHT_DICT = {"kale": 0.3, "broccoli": 0.2, "apple": 0.4}
+
+# Create a dictionary with each exercise that the user can choose from
+EXERCISE_DICT = {1: "hopping", 2: "running", 3: "walking"}
