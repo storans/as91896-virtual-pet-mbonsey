c57073ec0f1dee72086b6f51b1a42c410cab5642
MBonsey <mbonsey@storans.school.nz>
2019-10-27 14:01:18 +1300

    Ask the user to enter their name and set this as the variable user_name

--- user_name.py
+++ user_name.py
@@ -1,4 +1,3 @@
 # Ask the user to enter their name and set this as the variable user_name
 user_name = input("Welcome to Virtual Pet! Please type your name here: ").strip().capitalize()
-
 print(user_name)
