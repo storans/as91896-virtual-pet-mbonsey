e0bf5c980c3b990803541fc804a733f0012b44d3
mbonsey <55725868+mbonsey@users.noreply.github.com>
2019-10-27 13:58:22 +1300

    Delete welcome.py

--- welcome.py
+++
@@ -1,7 +0,0 @@
-# Ask the user to enter their name and set this as user_name
-user_name = input("Welcome to Virtual Pet! Please type your name here: ").strip().capitalize()
-print()
-
-# Create a welcome message and give them information on how to use the program
-print("Hello, {} and welcome to Virtual Pet! This game allows you to look after your very own pet rabbit that you can feed and exercise.".format(user_name))
-print()
