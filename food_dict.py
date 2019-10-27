2581e376aeb0dee0c57f39e9d8bc69e9eb7172f1
MBonsey <mbonsey@storans.school.nz>
2019-10-27 14:46:51 +1300

    Create a dictionary with each food and the number of kilograms it will add to the weight of the rabbit

--- food_dict.py
+++ food_dict.py
@@ -1,4 +1,2 @@
 # Create a dictionary with each food and the number of kilograms it will add to the weight of the rabbit
 FOOD_DICT = {'kale': 0.3, 'broccoli': 0.2, 'apple': 0.4}
-
-print(FOOD_DICT)
