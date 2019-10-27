3390c03abd44b8638b359893b409e994836a2369
MBonsey <mbonsey@storans.school.nz>
2019-10-27 14:52:48 +1300

    Create a dictionary with each food that the user can choose from

--- main_menu.py
+++ main_menu.py
@@ -1,2 +1,5 @@
 # Create a dictionary of different things eg. weight and exercise, that user can check about their rabbit
 MAIN_DICT = {1: "feed", 2: "exercise", 3: "check weight", 4: "end game", 5: "help"}
+
+# Create a dictionary with each food that the user can choose from
+FOOD_DICT = {1: "kale", 2: "broccoli", 3: "apple"}
