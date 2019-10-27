e67c040b8018ddb9c78dec889e4fea3a17bb44b5
mbonsey <55725868+mbonsey@users.noreply.github.com>
2019-10-27 13:56:38 +1300

    Delete exercise_dict.py

--- exercise_dict.py
+++
@@ -1,5 +0,0 @@
-# Create a dictionary with each exercise and the number of kilograms it will subtract to the weight of the rabbit
-EXERCISE_DICT = {'hopping': 0.3, 'running': 0.5, 'walking': 0.1}
-
-print(EXERCISE_DICT)
-#
