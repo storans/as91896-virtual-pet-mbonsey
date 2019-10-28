bf40aa1bd89de1219b9250543f019859601d4332
MBonsey <mbonsey@storans.school.nz>
2019-10-28 14:13:14 +1300

    Add my integer checking function to this code in order to check that the user inputs a valid number when choosing an action from the main menu

--- main_menu.py
+++ main_menu.py
@@ -1,3 +1,21 @@
+# Create a universal integer checking function
+# Question and error allow it to be used in a range of situations
+def check_int(question, error, low, high):
+    valid = False
+    while not valid:
+        # Any question that needs a number
+        number = input("{}: ".format(question))
+        try:
+            number = int(number)
+            if low <= number <= high:
+                return number
+            else:
+                print(error)
+        # If an invalid input is made, the prompt is made again
+        except ValueError:
+            # The error that prints is customised to the question
+            print(error)
+
 # Create a dictionary of different things eg. weight and exercise, that user can check about their rabbit
 MAIN_DICT = {1: "feed", 2: "exercise", 3: "check weight", 4: "end game", 5: "help"}
 
